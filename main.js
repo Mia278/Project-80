07c7735839d1f6e55d12c0908923209f63ec8bfa
Mia278 <74997937+Mia278@users.noreply.github.com>
2021-02-10 19:15:24 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,49 +1,20 @@
-name_of_student_array = [];
-
-function submit()  {
-  var display_student_array = [];
-  for (var j = 1; j <= 4;j++)
-  {
-      var name_of_student = document.getElementById("name_"+j).value;
-      console.log(name_of_student);
-      name_of_student_array.push(name_of_student);
-  }
-    console.log(name_of_student_array);
-    var length_of_name_of_student =  name_of_student_array.length;
-    console.log(length_of_name_of_student);
-
-    for ( var k=0; k< length_of_name_of_student; k++){
-        display_student_array.push("name-" name_of_student_array [k]);
-        console.log(display_student_array);
-    }
-        console.log(display_student_array); 
-        document.getElementById("display_names_with_commas").innerHTML = display_student_array;
-    var remove_commas =display_student_array.join(" ");
-    console.log(remove_commas);
-    document.getElementById("display_name_without_commas").innerHTML = remove_commas;
 
 
-    document.getElementById("submit_button").style.display = "none";
-    document.getElementById("sort_button").style.display = "inline-block";
+function getParagraph1() {
+    var inputs = [] ;
 
-
+for (var i  = 1 ;  i<= 6 ; i++)
+ inputs.push (document.getElementById("para1_input_box_" + i).value) ;
+ inputs.join(". ");
+ document.getElementById("showParagraph1").innerHTML = inputs.join(". ")
 }
 
-function sorting() {
-   name_of_student_array.sort();
-    console.log(name_of_student_array);
-    var displaystudentarraysorting = [];
-    var length_of_name_of_studentsarray = name_of_student_array.length;
-    console.log(length_of_name_of_studentsarray);
-     
-     for (var k = 0; k< length_of_name_of_studentsarray; k++);
-       {
-               displaystudentarraysorting.push("name-" +name_of_student_array [k] );
-               console.log(displaystudentarraysorting);
-       } 
-    
-    var remove_commas = displaystudentarraysorting.join(" ");
-    console.log(remove_commas);
-    document.getElementById("display_name_without_commas").innerHTML = remove_commas;
 
+function getParagraph2() {
+    var inputs2 = [] ;
+
+for (var m  = 1 ;  m<= 6 ; m++)
+ inputs2.push (document.getElementById("para1_input_box_" + m).value) ;
+ inputs2.join(". ");
+ document.getElementById("showParagraph2").innerHTML = inputs2.join(". ")
 }
